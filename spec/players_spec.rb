1bb634712fe63942eb7b3e393d2247bc03f16294
Daniduran-dev <danigduran@gmail.com>
2020-09-17 22:11:37 +0700

    players methods tested

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -2,15 +2,22 @@ require_relative '../lib/players.rb'
 
 describe Players do
   let(:players) { Players.new('Dani', 'Duran') }
+
   describe '#designation' do
-    players.desigantion
-    expect(plays_with).to eql('Dani plays with X    Duran plays with O')
+    it 'returns a string with the names of the players and their marks' do
+      expect(players.designation).to eql('Dani plays with X    Duran plays with O')
+    end
   end
 
   describe '#turn' do
-    players.turn(0)
     it 'returns one of the 2 sentences depending on the round number' do
-      expect
+      expect(players.turn(0)).to eql "Dani it's your turn"
+    end
+  end
+
+  describe '#winner' do
+    it 'returns one of the 2 winner sentences depending on the round number' do
+      expect(players.winner(0)).to eql 'The winner is Dani'
     end
   end
 end
