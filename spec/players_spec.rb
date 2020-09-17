83fbdb4663bc1f1401c58862c94f229d38de8f3e
Daniduran-dev <danigduran@gmail.com>
2020-09-17 20:58:06 +0700

    players_designation tested

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -1,12 +1,15 @@
-require './lib/players'
+require_relative '../lib/players.rb'
+
 describe Players do
   let(:players) { Players.new('Dani', 'Duran') }
   describe '#designation' do
-    expect(players.designation).to eql('Dani plays with X    Duran plays with O')
+    players.desigantion
+    expect(plays_with).to eql('Dani plays with X    Duran plays with O')
   end
-  describe "#turn" do
+
+  describe '#turn' do
     players.turn(0)
-    it "returns one of the 2 sentences depending on the round number" do
+    it 'returns one of the 2 sentences depending on the round number' do
       expect
     end
   end
