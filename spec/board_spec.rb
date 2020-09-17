a004c0ff30060f7742d2506141d7df03e70d39e5
Daniduran-dev <danigduran@gmail.com>
2020-09-17 17:29:34 +0700

    update_board && validate_position tested

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,31 +1,40 @@
-require './lib/board'
+require_relative '../lib/board.rb'
+
 describe Board do
   let(:board) { Board.new }
-  let(:position) {:a1}
-  let(:round) {0}
-  describe "#display_board" do
-    it "returns the string stored in the variable" do
-      mini_board = board.display_board
-      expect(mini_board).to be_a(String)
-    end
-  end
-  describe "#empty_cells" do
-    it "shows an array with the keys representing the free positions" do
-      mini_board = board.empty_cells
-      expect(mini_board).to be_a(Array)
-    end
-  end
-  describe "#update_board" do
-    it "sets an 'X' or a 'O' in a board position given depending if the round is even or not" do
-      position 
-      move = board[position]
-      expect(move).to eql('X')
-    end
-  end
-  describe "#validate_position" do
 
+  describe '#display_board' do
+    it 'checks if there is a string' do
+      expect(board.display_board).to be_a(String)
+    end
+    it 'displays the board on screen' do
+      expect(board.display_board).to eql "   1   2   3\na|   |   |   |\nb|   |   |   |\nc|   |   |   |"
+    end
   end
-  describe "#winner_checking" do
-    
+
+  describe '#empty_cells' do
+    it 'shows an array with the keys representing the free positions' do
+      expect(board.empty_cells).to be_a(Array)
+    end
+    it 'returns an array with the symbols of the free positions' do
+      expect(board.empty_cells).to eql %i[a1 a2 a3 b1 b2 b3 c1 c2 c3]
+    end
+  end
+  # rubocop: disable Layout/LineLength
+  describe '#update_board' do
+    it "sets an 'X' or a 'O' in a board position given depending if the round is even or not" do
+      board.update_board(:a1, 0)
+      expect(board.instance_variable_get(:@board)).to eql({ a1: 'X', a2: ' ', a3: ' ', b1: ' ', b2: ' ', b3: ' ', c1: ' ', c2: ' ', c3: ' ' })
+    end
+  end
+  # rubocop: enable Layout/LineLength
+  describe '#validate_position' do
+    it 'returns true if the position is available and within the board' do
+      # board.validate_position(:a1)
+      expect(board.validate_position(:a1)).to eql true
+    end
+  end
+  describe '#winner_checking' do
+
   end
 end
