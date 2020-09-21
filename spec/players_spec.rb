98e1aca68dd208f78fd561c8e3945fe8565def42
Duran <danigduran@gmail.com>
2020-09-21 20:59:12 +0700

    different names and minimum lenght of name tested

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -2,7 +2,18 @@ require_relative '../lib/players.rb'
 
 describe Players do
   let(:players) { Players.new('Dani', 'Duran') }
-
+  let(:player1) { 'Dani' }
+  let(:player2) { 'Duran' }
+  describe '#initialize' do
+    players = Players.new('Dani', 'Duran')
+    it 'checks if the names have at least 1 charachter' do
+      expect(player1.size).to be >= 1
+      expect(player2.size).to be >= 1
+    end
+    it 'checks if the names of the players are different' do
+      expect(player1).not_to eql player2
+    end
+  end
   describe '#designation' do
     it 'returns a string with the names of the players and their marks' do
       expect(players.designation).to eql('Dani plays with X    Duran plays with O')
