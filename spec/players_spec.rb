ff339a9da75f21aebbe9cf62d864a9f5deb07212
Duran <danigduran@gmail.com>
2020-09-21 16:24:50 +0700

    fix players_spec.rb

--- spec/players_spec.rb
+++ spec/players_spec.rb
@@ -6,6 +6,8 @@ describe Players do
   describe '#designation' do
     it 'returns a string with the names of the players and their marks' do
       expect(players.designation).to eql('Dani plays with X    Duran plays with O')
+    end
+    it 'checks if the player names are not showing' do
       expect(players.designation).not_to eql(' plays with X     plays with O')
     end
   end
@@ -13,6 +15,8 @@ describe Players do
   describe '#turn' do
     it 'returns one of the 2 sentences depending on the round number' do
       expect(players.turn(0)).to eql "Dani it's your turn"
+    end
+    it 'checks that takes the right player' do
       expect(players.turn(0)).not_to eql "Duran it's your turn"
     end
   end
@@ -20,6 +24,8 @@ describe Players do
   describe '#winner' do
     it 'returns one of the 2 winner sentences depending on the round number' do
       expect(players.winner(0)).to eql 'The winner is Dani'
+    end
+    it 'checks that takes the right player' do
       expect(players.winner(0)).not_to eql 'The winner is Duran'
     end
   end
